aab7034920856e57d30c732f7808a2b9defbe370
whoscared <senatorova.ss@edu.spbstu.ru>
2022-10-22 22:36:31 +0300

    homework

--- src/main/CheckAnswer.java
+++ src/main/CheckAnswer.java
@@ -1,13 +1,14 @@
 package main;
 
-import tasks.chess.Chess;
+import tasks.homework.Homework;
 import tasks.sequence.CharacterSequence;
-import tasks.sequence.NumericalSequence;
-import tasks.sort.NumbersSort;
+
+import java.util.Arrays;
 
 public class CheckAnswer {
     public static void main(String[] args) {
-        System.out.println(CharacterSequence.frequencyGraph("Hello, Sunny!!<3"));
+        System.out.println(Arrays.toString(Homework.subway(2, 2, 3, 4)));
+        System.out.println(Arrays.toString(Homework.searchHome(11,21,2,3,4)));
 
     }
 }
